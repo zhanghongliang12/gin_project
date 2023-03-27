952b561a6113527f65e1726461385ac37febf5a8
ysh <ysh@et.cn>
2023-03-27 13:36:45 +0800

    2

--- app/demo/routers.go
+++ app/demo/routers.go
@@ -3,10 +3,10 @@ package demo
 import "github.com/gin-gonic/gin"
 
 func DemoRouter(r *gin.Engine) {
-	r.Group("/demo")
+	demo := r.Group("/demo")
 	{
-		r.POST("/form", FromHandler)
-		r.POST("/cookie", FromHandler)
+		demo.POST("/form", FromHandler)
+		demo.POST("/cookie", FromHandler)
 
 	}
 
